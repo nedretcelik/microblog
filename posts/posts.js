a59c3c35cd2c11f2ab270ac353321280a82a42df
nedretcelik <nedretcelik97@gmail.com>
2024-06-24 23:54:05 +0300

    add left and right sticky class and content

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,19 @@
-/* Posts Page JavaScript */
 
 "use strict";
+
+
+/* POST http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts
+Content-Type: application/json
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5lZHJldGMiLCJpYXQiOjE3MTkyNDYyMzUsImV4cCI6MTcxOTMzMjYzNX0.NXGcQesfxMatmMkTIBONacMx8FpxRPXjindbxTm0t0E
+
+{
+  "text": "Weather is perfect today!"
+} */
+
+
+/* 
+GET http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/
+Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5lZHJldGMiLCJpYXQiOjE3MTkyNDYyMzUsImV4cCI6MTcxOTMzMjYzNX0.NXGcQesfxMatmMkTIBONacMx8FpxRPXjindbxTm0t0E */
+
+
+
